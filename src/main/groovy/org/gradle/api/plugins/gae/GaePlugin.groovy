1b91d406cc1a20f8a6263926db25cdf469ab7cff
Marcin Erdmann <erdi84@gmail.com>
2012-07-10 19:00:45 +0100

    Fixed functionalTest source set dependencies for eclipse projects

--- src/main/groovy/org/gradle/api/plugins/gae/GaePlugin.groovy
+++ src/main/groovy/org/gradle/api/plugins/gae/GaePlugin.groovy
@@ -33,6 +33,7 @@ import org.gradle.api.plugins.*
 import org.gradle.api.plugins.gae.task.*
 import org.gradle.api.plugins.gae.task.appcfg.*
 import org.gradle.api.plugins.gae.task.appcfg.backends.*
+import org.gradle.plugins.ide.eclipse.EclipsePlugin
 
 /**
  * <p>A {@link Plugin} that provides tasks for uploading, running and managing of Google App Engine projects.</p>
@@ -462,6 +463,7 @@ class GaePlugin implements Plugin<Project> {
         functionalSourceSet.runtimeClasspath = functionalSourceSet.output + functionalTestRuntimeConfiguration
 
         addIdeaConfigurationForFunctionalTestSourceSet(project, functionalTestCompileConfiguration, functionalTestRuntimeConfiguration, functionalSourceSet)
+        addEclipseConfigurationForFunctionalTestRuntimeConfiguration(project, functionalTestRuntimeConfiguration)
 
         functionalSourceSet
     }
@@ -473,6 +475,12 @@ class GaePlugin implements Plugin<Project> {
                 scopes.TEST.plus += compile
                 scopes.TEST.plus += runtime
             }
+        }
+    }
+
+    private void addEclipseConfigurationForFunctionalTestRuntimeConfiguration(Project project, Configuration functionalTestRuntimeConfiguration) {
+        project.plugins.withType(EclipsePlugin) { EclipsePlugin plugin ->
+            plugin.model.classpath.plusConfigurations += functionalTestRuntimeConfiguration
         }
     }
 
